aa950b789ae25a6e33ae1c63d39955f9508b1717
Luke Grunau <lukegrunau@gmail.com>
2020-04-25 13:58:14 +0200

    working on map info window content

--- frontend/src/views/Map.vue
+++ frontend/src/views/Map.vue
@@ -11,7 +11,7 @@
             <input
                 type="text"
                 id="textSearch"
-                placeholder="Search for a type of place"
+                placeholder="Search for a place"
                 v-model="textSearchQuery"
             />
             <button @click.prevent="textSearch">Submit</button>
@@ -77,20 +77,20 @@ export default {
         }
     },
     methods: {
-        setMapOnAll: function(map) {
+        setMapOnAll(map) {
             this.markers.forEach(marker => marker.setMap(map));
         },
-        clearMarkers: function() {
+        clearMarkers() {
             this.setMapOnAll(null);
         },
-        showMarkers: function() {
+        showMarkers() {
             this.setMapOnAll(this.map);
         },
-        deleteMarkers: function() {
+        deleteMarkers() {
             this.clearMarkers();
             this.markers = [];
         },
-        addMarkers: function(places) {
+        addMarkers(places) {
             places.forEach(place => {
                 let marker = new google.maps.Marker({
                     position: place.geometry.location,
@@ -105,12 +105,12 @@ export default {
                 });
             });
         },
-        createInfoWindow: function() {
+        createInfoWindow() {
             // storing in data property ensures that only one info window can be open at any time
             let infowindow = new google.maps.InfoWindow();
             this.infoWindow = infowindow;
         },
-        placeSearch: function() {
+        placeSearch() {
             this.deleteMarkers();
             axios
                 .get(
@@ -128,7 +128,7 @@ export default {
                     console.log("Error in place search: ", err);
                 });
         },
-        textSearch: function() {
+        textSearch() {
             this.deleteMarkers();
             axios
                 .get(
@@ -144,11 +144,11 @@ export default {
                     console.log("Error in text search: ", err);
                 });
         },
-        getPlaceDetails: function(place) {
+        getPlaceDetails(place) {
             // pan to location, set zoom
             console.log("place: ", place.name);
             this.map.panTo(place.geometry.location);
-            // this.map.setZoom(16);
+            this.map.setZoom(14);
             // call backend
             axios
                 .get(
@@ -162,26 +162,81 @@ export default {
                     console.log("Marker position: ", this.markers[0].position);
                     let markerMatch = this.markers.find(
                         marker =>
-                            marker.position.lat() ==
-                                this.placeDetails.geometry.location.lat &&
-                            marker.position.lng() ==
-                                this.placeDetails.geometry.location.lng
+                            marker.position.lat().toFixed(7) ==
+                                this.placeDetails.geometry.location.lat.toFixed(
+                                    7
+                                ) &&
+                            marker.position.lng().toFixed(7) ==
+                                this.placeDetails.geometry.location.lng.toFixed(
+                                    7
+                                )
                     );
                     console.log("marker match found? ", markerMatch);
-                    this.infoWindow.setContent(
-                        `<div><strong>${this.placeDetails.name}</strong></div>`
-                    );
+                    let content = this.getInfoWindowContent(this.placeDetails);
+                    this.infoWindow.setContent(content);
                     this.infoWindow.open(this.map, markerMatch);
                 })
                 .catch(err => {
                     console.log("Error getting place details: ", err);
                 });
+        },
+        getInfoWindowContent(place) {
+            let dynamicContent = "";
+            // business_status field not being returned for some reason
+            // if (place.business_status == "CLOSED_TEMPORARILY") {
+            //     dynamicContent += `
+            //     <h3 class="closed">This business is currently marked as temporarily closed due to COVID-19.</h3>
+            //     `;
+            // }
+            for (let i = 0; i < place.types.length; i++) {
+                if (place.types[i] == "park") {
+                    dynamicContent += `
+                            <h2 class="open">Open</h2>
+                            <h3>Parks are open to the public, provided that social distance is maintained. On permanently installed seating (such as benches), the minimum distance is 1.5 m. In meadows and open spaces, the minimum distance is 5 m.</h3>`;
+                    break;
+                }
+                if (place.types[i] == "museum") {
+                    dynamicContent += `
+                            <h2 class="soon-open">Permitted to open from 04 May, 2020</h2>
+                            <h3>Go and reflect on past civilizations while keeping your distance to preserve the current one.</h3>
+                        `;
+                    break;
+                }
+                if (place.types[i] == "hair_care") {
+                    dynamicContent += `
+                        <h2 class="soon-open">Permitted to open from 04 May, 2020</h2>
+                        <h3>Finally you can get a haircut!</h3>
+                    `;
+                    break;
+                }
+                if (
+                    place.types[i] == "supermarket" ||
+                    place.types[i] == "pharmacy" ||
+                    place.types[i] == "convenience_store" ||
+                    place.types[i] == "grocery_or_supermarket" ||
+                    place.types[i] == "drugstore"
+                ) {
+                    dynamicContent += `
+                        <h2 class="open">Open</h2>
+                        <h3>All retail outlets selling essential supplies such as food and medication will remain open</h3>
+                    `;
+                    break;
+                }
+            }
+
+            const content = `
+            <div class="info-window-content">
+                <h1>${place.name}</h1>
+                <div class="dynamic-content">${dynamicContent}</div>
+            </div>
+            `;
+            return content;
         }
     }
 };
 </script>
 
-<style scoped>
+<style>
 .map-container {
     display: flex;
     height: 600px;
@@ -214,5 +269,20 @@ export default {
     height: 100%;
     width: 60%;
     justify-self: right;
+}
+
+.info-window-content {
+}
+
+.open {
+    color: green;
+}
+
+.soon-open {
+    color: orange;
+}
+
+.closed {
+    color: red;
 }
 </style>
